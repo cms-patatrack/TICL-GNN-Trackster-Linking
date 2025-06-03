7d3915be0cfc4d84203e1e396f99272b392bb002
chrisizeh <me@christinezeh.com>
2025-06-03 15:46:10 +0200

    remove tensorboard as dependency

--- GNN_TrackLinkingNet.py
+++ GNN_TrackLinkingNet.py
@@ -2,7 +2,6 @@ import numpy as np
 
 import torch
 import torch.nn as nn
-from torch.utils.tensorboard import SummaryWriter
 import torch.nn.functional as F
 
 from EdgeConvBlock import EdgeConvBlock
