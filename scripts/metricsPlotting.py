e08cde666a1fbd184407f6c3e5fe734f358b4197
Christine Zeh <zeh.chrisi@gmail.com>
2025-09-19 16:07:34 +0200

    clean up script

--- scripts/metricsPlotting.py
+++ scripts/metricsPlotting.py
@@ -1,54 +1,31 @@
 import os.path as osp
 import os
-from datetime import datetime
-import json
 from glob import glob
 
 import awkward as ak
 
 import torch
-from torch import jit
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from torch_geometric.loader.dataloader import DataLoader
 import matplotlib.pyplot as plt
 
 from tracksterLinker.datasets.NeoGNNDataset import NeoGNNDataset
-from tracksterLinker.utils.dataStatistics import *
-from tracksterLinker.utils.graphUtils import *
-from tracksterLinker.utils.graphMetric import *
 from tracksterLinker.utils.graphHeatMap import GraphHeatmap
 
-model_name = "model-08-21"
 
-base_folder = "/home/czeh"
-model_folder = osp.join(base_folder, "GNN/model")
-output_folder = "/eos/user/c/czeh/stabilityCheck"
-hist_folder = osp.join(base_folder, "GNN/full_PU")
-data_folder = osp.join(base_folder, "GNN/datasetPU")
-os.makedirs(model_folder, exist_ok=True)
-
-# Prepare Dataset
-batch_size = 1
-dataset = NeoGNNDataset(data_folder, hist_folder, test=True)
-data_loader = DataLoader(dataset, shuffle=True, batch_size=batch_size)
+output_folder = "/home/czeh/newGNNMetrics"
 
 # CUDA Setup
 device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
 # device = torch.device("cpu")
 print(f"Using device: {device}")
 
-# Prepare Model
-model = jit.load(osp.join(model_folder, f"{model_name}.pt"))
-model = model.to(device)
-model.eval()
-
-files = glob(osp.join(output_folder, "metrics", f"graph_*.pt"))
+files = glob(osp.join(output_folder, f"*.pt"))
 hm_dU_signal = GraphHeatmap(resolution=250)
 hm_dO_signal = GraphHeatmap(resolution=250)
 hm_dU_PU = GraphHeatmap(resolution=250)
 hm_dO_PU = GraphHeatmap(resolution=250)
 
 i = 0
+print(files)
 
 for file in files:
     print(file)
