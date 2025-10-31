efadada5022f1a63bddb99772136ef34f3cd117e
Christine Zeh <zeh.chrisi@gmail.com>
2025-10-31 10:06:55 +0100

    add gradient clipping to prevent exploding gradients

--- tracksterLinker/tracksterLinker/GNN/LossFunctions.py
+++ tracksterLinker/tracksterLinker/GNN/LossFunctions.py
@@ -34,8 +34,8 @@ class ContrastiveLoss(nn.Module):
 
     def forward(self, output1, output2, label):
         euclidean_distance = nn.functional.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean(((1 - label) * euclidean_distance +
-                                      label * torch.clamp(self.margin - euclidean_distance, min=0.0)))
+        loss_contrastive = torch.mean(((1 - label) * torch.pow(euclidean_distance, 2) +
+                                      label * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2)))
         return loss_contrastive
 
 
--- tracksterLinker/tracksterLinker/GNN/train.py
+++ tracksterLinker/tracksterLinker/GNN/train.py
@@ -39,6 +39,7 @@ def train(model, opt, loader, epoch, weighted="raw_energy", scores=False, emb_ou
 
         # back-propagate and update the weight
         loss.backward()
+        torch.nn.utils.clip_grad_value_(model.parameters(), clip_value=1.0)
         opt.step()
         epoch_loss += loss
 
